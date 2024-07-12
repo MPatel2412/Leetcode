42eea761be2ae92785d64aaa9e88e214705df82d
Manthan Patel <101737432+MPatel2412@users.noreply.github.com>
2024-07-12 17:30:38 +0530

    Time: 765 ms (14.89%), Space: 667.4 MB (5.15%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.cpp
+++ 1397-search-suggestions-system/1397-search-suggestions-system.cpp
@@ -66,6 +66,43 @@ class Trie{
         }
         return true;
     }
+    void DFS(vector<string> &ans,string temp,Node *node)
+    {
+        if(ans.size() == 3)
+            return ;
+        if(node->isEnd())
+        {
+            ans.push_back(temp);
+        }
+
+        for(char ch ='a'; ch <= 'z'; ch++)
+        {
+            if(node->containsKey(ch))
+            {
+                temp.push_back(ch);
+                DFS(ans,temp,node->get(ch));
+                temp.pop_back();
+            }
+        }
+    }
+    vector<string> getStringswithPre(string prefix)
+    {
+        vector<string> ans;
+        string temp = "";
+        Node *node = root;
+        for(int i=0; i < prefix.size(); i++)
+        {
+            if(node->containsKey(prefix[i]))
+            {
+                temp += prefix[i];
+                node = node->get(prefix[i]);
+            }
+            else
+                return ans;
+        }
+        DFS(ans,temp,node);
+        return ans;
+    }
 
 };
 
@@ -75,23 +112,18 @@ public:
         int n= products.size();
         vector<vector<string>> ans;
         sort(products.begin(),products.end());
-        
+        Trie t;
+
+        for(auto it : products)
+        {
+            t.insert(it);
+        }
         for(int i=0; i < searchWord.size(); i++)
         {
             string pre = searchWord.substr(0,i+1);
-            int index = 0;
-            vector<string> temp;
-            int cnt = 0;
-            while(index < n && temp.size() < 3)
-            {
-                if(pre == products[index].substr(0,i+1))
-                {
-                    temp.push_back(products[index]);
-                }
-                index++;
-            }
-            ans.push_back(temp);
+            ans.push_back(t.getStringswithPre(pre));
         }
+
         return ans;
     }
 };
