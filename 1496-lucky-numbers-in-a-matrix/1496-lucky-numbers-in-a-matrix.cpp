74e353acb95156f4919ba349a12ec4f02d858c22
Manthan Patel <101737432+MPatel2412@users.noreply.github.com>
2024-07-19 09:09:45 +0530

    Time: 15 ms (64.89%), Space: 14.3 MB (18.75%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<int> mini(m,INT_MAX);
-        vector<int> maxi(n,INT_MIN);
-        for(int i=0; i < m;i++)
-        {
-            for(int j=0; j < n;j++)
-            {
-                mini[i] = min(mini[i],matrix[i][j]);
-            }
-        }
-        for(int i=0; i < n;i++)
-        {
-            for(int j=0; j < m;j++)
-            {
-                maxi[i] = max(maxi[i],matrix[j][i]);
-            }
-        }
-        vector<int> ans;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        int rMinMax = INT_MIN;
+        for (int i = 0; i < n; i++) {
 
-        for(int i=0; i < m;i++)
-        {
-            for(int j=0; j < n;j++)
-            {
-                if((matrix[i][j] == mini[i]) && (matrix[i][j] == maxi[j]))
-                    ans.push_back(matrix[i][j]);
+            int rMin = INT_MAX;
+            for (int j = 0; j < m; j++) {
+                rMin = min(rMin, matrix[i][j]);
             }
+            rMinMax = max(rMinMax, rMin);
         }
-        return ans;
+        
+        int cMaxMin = INT_MAX;
+        for (int i = 0; i < m; i++)
+        {
+            int cMax = INT_MIN;
+            for (int j = 0; j < n; j++) 
+            {
+                cMax = max(cMax, matrix[j][i]);
+            }
+            cMaxMin = min(cMaxMin, cMax);
+        }
+        
+        if (rMinMax == cMaxMin) {
+            return {rMinMax};
+        }
+        
+        return {};
     }
 };
