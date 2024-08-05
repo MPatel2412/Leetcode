b2310e836f927b57afb2e9943cc173601abf02bc
Manthan Patel <101737432+MPatel2412@users.noreply.github.com>
2024-08-05 23:11:18 +0530

    Time: 836 ms (77.09%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 select Department, Employee, Salary 
-From (select d.name as Department, e.id as ID, e.name as Employee, e.salary as Salary, DENSE_RANK() OVER (PARTITION BY d.id ORDER by e.salary desc) as ranking FROM Employee e, Department d WHERE d.id = e.departmentId ORDER by d.id, e.salary) as ED 
+From (select d.name as Department, e.id as ID, e.name as Employee, e.salary as Salary, DENSE_RANK() OVER (PARTITION BY d.id ORDER by e.salary desc) as ranking FROM Employee e, Department d WHERE d.id = e.departmentId ORDER by d.id, e.salary) ED 
 where ranking < 4;
