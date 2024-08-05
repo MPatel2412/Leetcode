f88082a8e779b5f9bf5fbc35c5536bc194bf4be6
Manthan Patel <101737432+MPatel2412@users.noreply.github.com>
2024-08-05 23:08:59 +0530

    Time: 740 ms (90.85%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select Department, Employee, Salary 
+From (select d.name as Department, e.id as ID, e.name as Employee, e.salary as Salary, DENSE_RANK() OVER (PARTITION BY d.id ORDER by e.salary desc) as ranking FROM Employee e, Department d WHERE d.id = e.departmentId ORDER by d.id, e.salary) as ED 
+where ranking < 4;
